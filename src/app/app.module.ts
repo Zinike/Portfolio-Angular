1aa94e4afcd1f7b2e0b34ec18cfbdf639b53b4c7
Zinike <marcoszinga_1994@hotmail.com>
2023-05-17 12:53:00 -0300

    2commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { EducacionComponent } from './educacion/educacion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule
